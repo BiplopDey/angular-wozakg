1a62af4f4eb6efcc76ce070caa61c073e3b28146
Biplop Dey <deybiplop4@gmail.com>
2022-03-28 14:24:38 +0200

    Can add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -13,6 +13,7 @@ import { ProductComponent } from './product/product.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    FormsModule,
     RouterModule.forRoot([
       { path: '', component: ShoppingListComponent },
     ])
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,12 +7,17 @@ import { Product } from '../product/product';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  list: Array<Product> = [{name:"Apple", cuantity:2}, {name:"Pear", cuantity:3}];
-  
+  list!: Array<Product>
+  productToAdd: Product = {name:"",cuantity:1}
   constructor() { 
   }
 
   ngOnInit(): void {
+    this.list = [{name:"Apple", cuantity:2}, {name:"Pear", cuantity:3}]
   }
 
+  add(): void {
+    this.list = [this.productToAdd, ...this.list]
+    this.productToAdd = {name:"",cuantity:1}
+  }
 }
